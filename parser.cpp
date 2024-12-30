9fd452a5aefb744389266e480bf423edf6f1d09a
Justxd22 <xdjust18@gmail.com>
2024-12-30 21:47:02 +0200

    - Add Json error handling - Add stdin support for large qml files

--- parser.cpp
+++ parser.cpp
@@ -6,6 +6,17 @@
 #include <iostream>
 #include <sstream>
 #include <time.h>
+#include <QJsonObject>
+#include <QJsonDocument>
+
+
+#ifdef _WIN32
+#include <io.h>
+#define STDIN_FILENO 0
+#else
+#include <sys/select.h>
+#include <unistd.h>
+#endif
 
 #include <private/qqmljsengine_p.h>
 #include <private/qqmljslexer_p.h>
@@ -35,10 +46,18 @@ int Foobar::InternalRun(const QString &code) {
     const auto diagnosticMessages = parser.diagnosticMessages();
 
     for (const QQmlJS::DiagnosticMessage &m : diagnosticMessages) {
-      qWarning("%d: %s", m.loc.startLine, qPrintable(m.message));
+      // qWarning("Qml Parsing Error %d: %s", m.loc.startLine, qPrintable(m.message));
+      QJsonObject errorObject;
+      errorObject["line"] = static_cast<int>(m.loc.startLine);
+      errorObject["message"] = m.message;
+      errorObject["error"] = "Qml Parsing Error";
+
+      QJsonDocument jsonDoc(errorObject);
+      QString jsonString = jsonDoc.toJson(QJsonDocument::Compact);
+      std::cout << jsonString.toStdString();
     }
 
-    return 1;
+    return 0;
   }
 
   const bool debug = this->m_options.testFlag(Option::Debug);
@@ -62,40 +81,56 @@ int Foobar::InternalRun(const QString &code) {
 Foobar::Foobar(Options options) { this->m_options = options; }
 
 int Foobar::Run(QStringList args) {
-  if (args.count() == 0) {
-    QTextStream(stderr) << "Please provide a path or a QML text\n";
-    return 1;
-  }
+    QString code;
 
-  if (args.count() > 1) {
-    QTextStream(stderr) << "Please provide only one path or one QML text\n";
-    return 1;
-  }
-
-  int returnValue = 0;
-  const QString pathOrText = args[0];
-
-  QFileInfo fileInfo(pathOrText);
-
-  if (fileInfo.isDir()) {
-    QTextStream(stderr) << "qml-parser doesn't hangle path to directory\n";
-    return 1;
-  }
-
-  if (fileInfo.isFile()) {
-    QFile file(pathOrText);
-    file.open(QFile::ReadOnly | QFile::Text);
-    const QString code = QString::fromUtf8(file.readAll());
-
-    returnValue = this->InternalRun(code);
-  } else {
-    QString code = pathOrText;
-    if (!pathOrText.trimmed().startsWith("import")) {
-      code = LZString::decompressFromBase64(code);
+    if (args.count() > 1) {
+        QTextStream(stderr) << "Please provide only one path or one QML text or use stdin.\n";
+        return 1;
     }
 
-    returnValue = this->InternalRun(code);
-  }
+    if (args.count() == 1) {
+        const QString pathOrText = args[0];
+        QFileInfo fileInfo(pathOrText);
 
-  return returnValue;
+        if (fileInfo.isDir()) {
+            QTextStream(stderr) << "qml-parser doesn't handle paths to directories.\n";
+            return 1;
+        }
+
+        if (fileInfo.isFile()) {
+            QFile file(pathOrText);
+            if (!file.open(QFile::ReadOnly | QFile::Text)) {
+                QTextStream(stderr) << "Error opening file: " << pathOrText << "\n";
+                return 1;
+            }
+            code = QString::fromUtf8(file.readAll());
+            file.close();
+        } else {
+            code = pathOrText;
+            if (!pathOrText.trimmed().startsWith("import")) {
+                code = LZString::decompressFromBase64(code);
+            }
+        }
+    } else { // No arguments provided, read from stdin if no stdin exit and report no input was provided 
+    #ifdef _WIN32
+    if (_isatty(_fileno(stdin))) {
+        QTextStream(stderr) << "Error: No input provided. Please provide input via stdin or command line arguments.\n";
+        return 1;
+    }
+    #else
+    struct timeval tv = {0, 0};
+    fd_set fds;
+    FD_ZERO(&fds);
+    FD_SET(STDIN_FILENO, &fds);
+    if (select(STDIN_FILENO + 1, &fds, NULL, NULL, &tv) <= 0) {
+        QTextStream(stderr) << "Error: No input provided. Please provide input via stdin or command line arguments.\n";
+        return 1;
+    }
+    #endif
+    
+    QTextStream in(stdin);
+    code = in.readAll();
+    }
+
+    return InternalRun(code);
 }
