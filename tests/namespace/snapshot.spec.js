f0263c064a5ef0c8c4c9331821bedaa79f3c9831
Justxd22 <xdjust18@gmail.com>
2025-02-07 20:41:43 +0200

    Update tests to ESM

--- tests/namespace/snapshot.spec.js
+++ tests/namespace/snapshot.spec.js
@@ -1,5 +1,11 @@
-const fs = require("fs");
-const { parse } = require("../../src");
+import fs from "fs";
+import { parse } from "../../src/index.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+// Get __dirname equivalent in ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 it("renders correctly", () => {
   const ast = parse(fs.readFileSync(`${__dirname}/entry.qml`).toString());
